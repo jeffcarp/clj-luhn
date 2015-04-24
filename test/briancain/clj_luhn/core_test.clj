f711c8dc6aadad67cea80804792cab9da4157e6f
Jeff Carpenter <gcarpenterv@gmail.com>
2015-04-23 22:16:24 -0700

    Add is function to tests to get running

--- test/briancain/clj_luhn/core_test.clj
+++ test/briancain/clj_luhn/core_test.clj
@@ -4,10 +4,10 @@
 
 (deftest valid?-test
   (testing "if a credit card number is valid"
-    (= true (valid? 49927398716)))
+    (is (valid? 49927398716)))
   (testing "if a credit card number is not valid"
-    (= false (valid? 79927398710))))
+    (is (not (valid? 79927398710)))))
 
 (deftest check-digit-test
   (testing "obtains the check digit from a card"
-    (= 3 (check-digit 79927398710))))
+    (is (= 3 (check-digit 79927398710)))))
